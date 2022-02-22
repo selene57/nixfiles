bec82ea858b6b503232a419f143fac168b2920bc
selene57 <selene57.dev@gmail.com>
2022-02-21 19:42:19 -0700

    nvim: updated lspconfig plugin with keybindings

--- config/nvim/lspconfig.lua
+++ config/nvim/lspconfig.lua
@@ -1,8 +1,21 @@
---setup global lspconfig keybindings or other settings
-
 --setup buffer specific functionality
 local on_attach = function()
 	--setup keybindings and completion (things that are buffer specific)
+        vim.cmd[[
+                augroup lsp_buf_format
+                au! BufWritePre <buffer>
+                autocmd BufWritePre <buffer> :lua vim.lsp.buf.formatting_sync()
+                augroup END
+        ]]
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=0})
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=0})
+        vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=0})
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=0})
+        vim.keymap.set("n", "<leader>dj", vim.diagnostic.goto_next, {buffer=0})
+        vim.keymap.set("n", "<leader>dk", vim.diagnostic.goto_prev, {buffer=0})
+        vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", {buffer=0})
+        vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {buffer=0})
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {buffer=0})
 end
 
 --setup all language servers in the list of servers below
