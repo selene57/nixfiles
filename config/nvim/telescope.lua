ab1ff6264d0b6bc0bd482c3b5e9f7ac7c24ece7c
selene57 <selene57.dev@gmail.com>
2022-02-22 01:51:53 -0700

    nvim: add keybinding for telescope plugin

--- config/nvim/telescope.lua
+++ config/nvim/telescope.lua
@@ -11,3 +11,9 @@ require('telescope').setup{
 }
 
 require('telescope').load_extension('fzf')
+
+vim.keymap.set('n', '<leader>fa', "<cmd>lua require('telescope.builtin').builtin()<cr>")
+vim.keymap.set('n', '<leader>ff', "<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<cr>")
+vim.keymap.set('n', '<leader>fq', "<cmd>lua require('telescope.builtin').quickfix()<cr>")
+vim.keymap.set('n', '<leader>fd', "<cmd>lua require('telescope.builtin').live_grep()<cr>")
+vim.keymap.set('n', '<leader>fb', "<cmd>lua require('telescope.builtin').buffers()<cr>")
